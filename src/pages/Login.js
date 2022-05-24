638886288b2dbadebefe25e36d8d342f255afb02
ed-cavalcanti <edney2809@gmail.com>
2022-05-24 16:00:15 -0300

    [feat] - req 01: incompleto

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,57 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      disabled: true,
+      nameInput: '',
+      emailInput: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.id]: target.value,
+    }, () => {
+      this.verifyButton();
+    });
+  }
+
+  verifyButton = () => {
+    const { nameInput, emailInput } = this.state;
+    if (nameInput.length > 0 && emailInput.length > 0) {
+      this.setState({ disabled: false });
+    }
+  }
+
   render() {
+    const { disabled } = this.state;
     return (
-      <h1>Login</h1>
+      <div>
+        <h1>Login</h1>
+        <section>
+          <label htmlFor="nameInput">
+            Player name:
+            <input
+              type="text"
+              id="nameInput"
+              data-testid="input-player-name"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="emailInput">
+            E-mail:
+            <input
+              type="email"
+              id="emailInput"
+              data-testid="input-gravatar-email"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button type="button" disabled={ disabled }>Play</button>
+        </section>
+      </div>
     );
   }
 }
