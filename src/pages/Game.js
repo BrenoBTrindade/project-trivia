b6a6635f05bac672e461b3273c94a6bcc67b4f62
Mariana Fernanda <ariana.fernanda@icloud.com>
2022-05-25 15:48:23 -0300

    [feat]: Requisito 6 incompleto

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,15 +1,78 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import Header from '../components/Header';
+import { fetchQuestions } from '../services/api';
 
 class Game extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Game</h1>
-        <Header />
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      responseCode: 0,
+      questions: [],
+      questionsIndex: 1,
+      loading: true,
+    };
   }
+
+  async componentDidMount() {
+    const token = localStorage.getItem('token');
+    const data = await fetchQuestions(token);
+    this.setState({ questions: data.results, responseCode: data.response_code }, () => {
+      this.setState({ loading: false });
+    });
+    this.tokenValidation();
+  }
+
+  tokenValidation = () => {
+    const { responseCode } = this.state;
+    const { history } = this.props;
+    const three = 3;
+    if (responseCode === three) {
+      localStorage.removeItem('token');
+      history.push('/');
+    }
+  };
+
+shuffleArray = () => {
+  const { questions, questionsIndex } = this.state;
+  const answers = [questions[questionsIndex].correct_answer,
+    ...questions[questionsIndex].incorrect_answers];
+  console.log(answers);
 }
+
+render() {
+  const { questions, questionsIndex, loading } = this.state;
+  return (
+    <div>
+      <h1>Game</h1>
+      <Header />
+      {loading && <h2>Loading...</h2>}
+      {!loading && (
+        <div>
+          <p
+            data-testid="question-category"
+          >
+            { questions[questionsIndex].category }
+          </p>
+          <p
+            data-testid="question-text"
+          >
+            { questions[questionsIndex].question }
+          </p>
+          <div>
+            { this.shuffleArray() }
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+}
+
+Game.propTypes = {
+  history: propTypes.shape({
+    push: propTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Game;
--- src/services/api.js
+++ src/services/api.js
@@ -5,4 +5,11 @@ const fetchToken = async () => {
   return result.token;
 };
 
+export const fetchQuestions = async (token) => {
+  const url = `https://opentdb.com/api.php?amount=5&token=${token}`;
+  const request = await fetch(url);
+  const result = await request.json();
+  return result;
+};
+
 export default fetchToken;
