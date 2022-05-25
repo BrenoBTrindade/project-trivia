d0676a7dfb54b83742eab4f5f72452719b349450
BrenoBTrindade <brenokaminski@hotmail.com>
2022-05-25 20:32:46 -0300

    [feat]: css pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,9 @@ import propTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import fetchToken from '../services/api';
+import logo from '../trivia.png';
 import { savePlayerName, savePlayerEmail } from '../redux/actions';
+import '../css/Login.css';
 
 class Login extends React.Component {
   constructor() {
@@ -45,40 +47,43 @@ class Login extends React.Component {
   render() {
     const { disabled } = this.state;
     return (
-      <div>
-        <h1>Login</h1>
-        <section>
-          <label htmlFor="nameInput">
-            Player name:
-            <input
-              type="text"
-              id="nameInput"
-              data-testid="input-player-name"
-              onChange={ this.handleChange }
-            />
-          </label>
-          <label htmlFor="emailInput">
-            E-mail:
-            <input
-              type="email"
-              id="emailInput"
-              data-testid="input-gravatar-email"
-              onChange={ this.handleChange }
-            />
-          </label>
-          <button
-            type="button"
-            onClick={ this.handleClick }
-            disabled={ disabled }
-            data-testid="btn-play"
-          >
-            Play
-          </button>
-          <Link to="/settings">
-            <button type="button" data-testid="btn-settings">Settings</button>
-          </Link>
-        </section>
-      </div>
+      <main className="login-main">
+        <img src={ logo } className="App-logo" alt="logo" />
+        <div className="login">
+          <h1>Login</h1>
+          <section className="form-login">
+            <label htmlFor="nameInput">
+              Player name:
+              <input
+                type="text"
+                id="nameInput"
+                data-testid="input-player-name"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <label htmlFor="emailInput">
+              E-mail:
+              <input
+                type="email"
+                id="emailInput"
+                data-testid="input-gravatar-email"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <button
+              type="button"
+              onClick={ this.handleClick }
+              disabled={ disabled }
+              data-testid="btn-play"
+            >
+              Play
+            </button>
+            <Link to="/settings">
+              <button type="button" data-testid="btn-settings">Settings</button>
+            </Link>
+          </section>
+        </div>
+      </main>
     );
   }
 }
