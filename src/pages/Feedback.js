6011b72732667a7707f69b710ebb8f09e847cf08
joao_victor_leal <joaovictorgusmaoleal@hotmail.com>
2022-05-27 14:51:28 -0300

    [feat]: requisito 15 completo

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -4,29 +4,40 @@ import propTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
-  render() {
-    const { assertions, score } = this.props;
+playAgain = () => {
+  const { history } = this.props;
+  history.push('/');
+};
 
-    const feedbackMessage = () => {
-      const THREE = 3;
-      if (assertions < THREE) {
-        return 'Could be better...';
-      }
-      return 'Well Done!';
-    };
+render() {
+  const { assertions, score } = this.props;
 
-    return (
-      <div>
-        <Header />
-        <h1>Feedback</h1>
-        <h2 data-testid="feedback-total-score">{ score }</h2>
-        <p data-testid="feedback-total-question">
-          {assertions}
-        </p>
-        <p data-testid="feedback-text">{ feedbackMessage() }</p>
-      </div>
-    );
-  }
+  const feedbackMessage = () => {
+    const THREE = 3;
+    if (assertions < THREE) {
+      return 'Could be better...';
+    }
+    return 'Well Done!';
+  };
+
+  return (
+    <div>
+      <Header />
+      <h1>Feedback</h1>
+      <h2 data-testid="feedback-total-score">{ score }</h2>
+      <p data-testid="feedback-total-question">
+        {assertions}
+      </p>
+      <p data-testid="feedback-text">{ feedbackMessage() }</p>
+      <input
+        type="button"
+        value="Play Again"
+        data-testid="btn-play-again"
+        onClick={ () => this.playAgain() }
+      />
+    </div>
+  );
+}
 }
 
 const mapStateToProps = (state) => ({
@@ -37,6 +48,9 @@ const mapStateToProps = (state) => ({
 Feedback.propTypes = {
   assertions: propTypes.number.isRequired,
   score: propTypes.number.isRequired,
+  history: propTypes.shape({
+    push: propTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default connect(mapStateToProps)(Feedback);
